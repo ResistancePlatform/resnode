7054f321c9d91449cc7b634d8f3fe98756752a81
Luke Wegryn <luke@resistance.io>
2019-02-06 15:57:18 +0000

    Added p2p

---
+++ p2p.js
@@ -0,0 +1,10 @@
+var P2P = require('socket.io-p2p');
+
+function p2pInit(socket){
+  var p2p = new P2P(socket);
+  p2p.on('peer-msg', function (data) {
+    console.log('From a peer %s', data);
+  });
+}
+
+module.exports = {p2pInit}
