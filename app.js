189d8060c04cffcdf4acfb73e9c7bdcc0c423187
Luke Wegryn <luke@resistance.io>
2019-02-07 18:14:34 +0000

    Added Dependencies

--- app.js
+++ app.js
@@ -25,8 +25,6 @@ if (!configuration) {
 const nodetype = configuration.active;
 const config = configuration[nodetype];
 
-require('./p2p.js')(config)
-
 if (config.ipv === '6') {
   console.log('You setup ipv6 connectivity. We need to apply a workaround for dns resolution.');
   require('./ipv6-dns-workaround');
