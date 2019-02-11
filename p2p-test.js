c28696d8b35bcf33e9e3dc2e1a875c2085971dda
Luke Wegryn <luke@resistance.io>
2019-02-11 16:02:22 +0000

    Registration Request Working

--- p2p-test.js
+++ p2p-test.js
@@ -9,12 +9,10 @@ setInterval(async function() {
   var peers = p2p.peers
   var registration = await p2p.getRegistration()
   var message = JSON.stringify({method: "register", message: registration.message, signature: registration.signature})
-  var message = JSON.stringify({method: "requestRegistration"))
+  var message = JSON.stringify({method: "requestRegistration"})
   for(var peer in peers){
-    //if(!peers[peer].registered){
-      if(peers[peer].conn){
-        await p2p.send(message, peers[peer].conn)
-      }
-    //}
+    if(!peers[peer].registered){
+      await p2p.send(message, peers[peer].conn)
+    }
   }
 }, 5000, p2p);
