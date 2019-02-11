83f26a4b475f04366406442e38fb3515c8425859
Luke Wegryn <luke@resistance.io>
2019-02-11 14:29:16 +0000

    Fixed Peer id checking

--- p2p.js
+++ p2p.js
@@ -107,7 +107,7 @@ class P2P {
       if (!this.peers[peerId]) {
         this.peers[peerId] = {}
       }
-      if(peerId.charAt(0) == 'r'){
+      if(peerId && peerId.substring(0,1) == 'r'){
 	this.peers[peerId].conn = conn
 	this.peers[peerId].seq = seq
 	this.connSeq++
